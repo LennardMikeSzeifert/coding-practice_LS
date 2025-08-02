1205bd6a01d985a99a622cbba62a2ebd5ca774ac
codeandtranslate <123456789+translatetodeutsch@gmail.com>
2025-08-02 20:08:25 +0200

    feat(js): set up base files for reverseString task
    - Create `README.md` to hold task instructions and notes.
    - Add `index.html` as a starting point for the project.
    - Create `script.js` for JavaScript implementation.
    
    Refs: javascript-exercises/07_reverseString
    Authored-by: Lennard

---
+++ array-tasks/task-07-reverseString/script.js
@@ -0,0 +1,27 @@
+/*
+
+Planning: 
+
+Does your program have a user interface? What will it look like? What functionality will the interface have? Sketch this out on paper.
+
+
+
+What inputs will your program have? Will the user enter data or will you get input from somewhere else?
+
+
+What’s the desired output?
+
+
+
+Given your inputs, what are the steps necessary to return the desired output?
+
+
+
+Pseudocode:
+
+
+
+*/
+
+// Do not edit below this line
+//module.exports = repeatString;
