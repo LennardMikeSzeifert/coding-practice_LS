6d1034645fcfad4c5fc0de78517726bdcac73e4a
codeandtranslate <123456789+translatetodeutsch@gmail.com>
2025-08-02 19:35:57 +0200

    feat(js): implement solution for repeatString task
    - Write code to repeat a given string a specified number of times.
    - Return the repeated string as a single combined result.
    
    Key learning:
    - Use `+=` to concatenate strings in a loop.
    
    Refs: javascript-exercises 06_repeatString
    Authored-by: Lennard

--- array-tasks/task-06-repeatString/script.js
+++ array-tasks/task-06-repeatString/script.js
@@ -4,33 +4,33 @@ Planning:
 
 Does your program have a user interface? What will it look like? What functionality will the interface have? Sketch this out on paper.
 
-- no interface.
-- will be completely in the console.
+- No interface.
+- Will be completely in the console.
 
 
 What inputs will your program have? Will the user enter data or will you get input from somewhere else?
 
-- the input will be a string and a number to define how often the string will be repeated for.
+- The input will be a string and a number to define how often the string will be repeated for.
 
 What’s the desired output?
 
-- a new string including the provided string repeated the number of times defined with the number.
+- A new string including the provided string repeated the number of times defined with the number.
 
 
 Given your inputs, what are the steps necessary to return the desired output?
 
-- write a function with two arguments (string, number)
-- wrtite a loop that adds the string as many times as the number defines
-- return the added up string
+- Write a function with two arguments (string, number)
+- Write a loop that adds the string as many times as the number defines
+- Return the added up string
 
 Pseudocode:
 
-function with arguments inputString and inputNumber
-define varibale let resultString as empty string
-FOR i is 0, i is smaller than inputNumber, increment i
-resultString +=  inputString
-ENDFOR
-return resultString
+Function repeatString(inputString, inputNumber)
+  Set resultString to empty string
+  For i from 0 to inputNumber (exclusive)
+    Add inputString to resultString
+  End For
+  Return resultString
 
 
 */
@@ -42,7 +42,7 @@ function repeatString(inputString, inputNumber) {
   }
   return resultString;
 }
-console.log(repeatString(`hello`, 10));
+console.log(repeatString(`hey`, 3));
 
 // Do not edit below this line
 //module.exports = repeatString;
