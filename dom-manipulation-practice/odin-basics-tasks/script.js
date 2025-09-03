ceaf65658fe8c7509c6c2b1ba50b3092615becea
codeandtranslate <123456789+translatetodeutsch@gmail.com>
2025-09-03 19:43:17 +0200

    feat(js): complete DOM manipulation task 2
    - Add a paragraph with text using DOM manipulation.
    - Style the paragraph with DOM manipulation.
    
    Refs: dom-manipulation-practice/odin-basics-tasks
    Authored-by: Lennard

--- dom-manipulation-practice/odin-basics-tasks/script.js
+++ dom-manipulation-practice/odin-basics-tasks/script.js
@@ -1,5 +1,5 @@
 // Task 1
-
+/*
 const para = document.createElement("p");
 
 para.textContent = "Hey I'm red!";
@@ -7,3 +7,14 @@ para.textContent = "Hey I'm red!";
 para.style.color = "red";
 
 document.body.appendChild(para);
+*/
+
+// Task 2
+
+const header = document.createElement("h3");
+
+header.textContent = "I'm a blue h3!";
+
+header.style.color = "blue";
+
+document.body.appendChild(header);
