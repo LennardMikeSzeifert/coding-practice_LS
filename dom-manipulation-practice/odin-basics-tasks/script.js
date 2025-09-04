173bcee993e21016ea5846caf64aa77578ff3b25
codeandtranslate <123456789+translatetodeutsch@gmail.com>
2025-09-04 09:27:37 +0200

    feat(js): complete DOM task 3 — add div with content
    - Add a div to the page using DOM manipulation.
    - Style the div with DOM manipulation.
    - Attach an h1 and a paragraph to the div with text using
      DOM manipulation.
    
    Refs: dom-manipulation-practice/odin-basics-tasks
    Authored-by: Lennard

--- dom-manipulation-practice/odin-basics-tasks/script.js
+++ dom-manipulation-practice/odin-basics-tasks/script.js
@@ -10,7 +10,7 @@ document.body.appendChild(para);
 */
 
 // Task 2
-
+/*
 const header = document.createElement("h3");
 
 header.textContent = "I'm a blue h3!";
@@ -18,3 +18,24 @@ header.textContent = "I'm a blue h3!";
 header.style.color = "blue";
 
 document.body.appendChild(header);
+*/
+
+// Task 3
+
+const div = document.createElement("div");
+
+div.style.cssText = "border: 2px solid black; background: pink";
+
+const header = document.createElement("h1");
+
+header.textContent = "I'm in a div";
+
+const para = document.createElement("p");
+
+para.textContent = "ME TOO!";
+
+div.appendChild(header, para);
+
+div.append(header, para);
+
+document.body.appendChild(div);
