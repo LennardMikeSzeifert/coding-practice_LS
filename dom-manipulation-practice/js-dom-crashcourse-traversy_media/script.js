ea109e14d9e871317ae14f0368cd552e41957131
codeandtranslate <123456789+translatetodeutsch@gmail.com>
2025-09-30 08:16:40 +0200

    feat(js): complete part 2 of DOM crash course
    - Learn about `parentNode` & `parentElement`.
    - Learn about `childNodes` & `firstElementChild`.
    - Learn about `nextElementSibling`.
    - Create a new element, add class, id, text, and insert it before the h1.
    
    Refs: dom-manipulation-practice/js-dom-crashcourse-traversy-media
    Authored-by: Lennard

--- dom-manipulation-practice/js-dom-crashcourse-traversy_media/script.js
+++ dom-manipulation-practice/js-dom-crashcourse-traversy_media/script.js
@@ -1,4 +1,6 @@
-console.dir(document);
+// PT. 1 //
+
+// console.dir(document);
 
 // GETELEMENTSBYCLASSNAME //
 
@@ -53,3 +55,58 @@ let odd = document.querySelectorAll(".list-group-item:nth-child(odd)");
 
 odd.forEach((item) => (item.style.backgroundColor = "lightgray"));
 */
+
+// PT. 2 //
+
+/*
+// parentNode & parentElement
+
+
+// itemList.parentNode.style.backgroundColor = "lightgray";
+
+itemList.parentElement.parentElement.parentElement.style.backgroundColor = "lightgray";
+*/
+
+// childNodes & elementChild
+
+// let itemList = document.querySelector("#items");
+
+// itemList.children[2].style.color = "yellow";
+
+// itemList.firstElementChild.style.backgroundColor = "yellow";
+
+// nextElementSilbilng
+
+// itemList.nextElementSibling.style.backgroundColor = "coral";
+
+// new element
+
+let newDiv = document.createElement("div");
+
+console.log(newDiv);
+
+// add class
+
+newDiv.className = "createdDiv";
+
+// add id
+
+newDiv.id = "div1";
+
+// add attribute
+
+newDiv.setAttribute("title", "the cloud");
+
+// create a text node
+
+let newDivText = document.createTextNode("Hello!");
+
+// add text to div
+
+newDiv.appendChild(newDivText);
+
+let container = document.querySelector("header .container");
+
+let h1 = document.querySelector("header h1");
+
+container.insertBefore(newDiv, h1);
