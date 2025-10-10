19b37c496f6cf3e2fc3f5e2db43e7a9e9949b30b
codeandtranslate <123456789+translatetodeutsch@gmail.com>
2025-10-10 09:02:56 +0200

    feat(js): complete part 4 of DOM crash course - final project
    - Learn how to add items to a list through DOM manipulation.
    - Learn how to remove items via button events.
    - Implement functionality to filter list items through an input field.
    
    Refs: dom-manipulation-practice/js-dom-crashcourse-traversy-media/final-project
    Authored-by: Lennard

--- dom-manipulation-practice/js-dom-crashcourse-traversy_media/final-project/project.js
+++ dom-manipulation-practice/js-dom-crashcourse-traversy_media/final-project/project.js
@@ -1,13 +1,96 @@
 const form = document.querySelector(`#addForm`);
 const itemList = document.querySelector(`#items`);
+let filter = document.querySelector(`.col-md-6 .form-control`);
 
 // Form submit event
 
-form.addEventListener(`sumbit`, addItem);
+form.addEventListener(`submit`, addItem);
 
-// Add item
+// Delete event
+
+itemList.addEventListener(`click`, removeItem);
+
+// Delete function
+
+function removeItem(e) {
+  if (e.target.classList.contains(`delete`)) {
+    if (confirm(`Are you sure?`)) {
+      e.target.parentElement.remove();
+    }
+  }
+}
+
+// Filter items
+
+filter.addEventListener(`keyup`, filterItem);
+
+// Filter function
+
+function filterItem(e) {
+  // Convert text to lower case
+
+  let filterInput = filter.value.toLowerCase();
+
+  // Get all lis of itemList
+
+  let lisItemList = itemList.querySelectorAll(`li`);
+
+  // Convert to array
+
+  lisItemList.forEach(function (item) {
+    let itemName = item.firstChild.textContent;
+
+    // Check match
+
+    if (itemName.toLowerCase().indexOf(filterInput) != -1) {
+      item.style.display = `block`;
+    } else {
+      item.style.display = `none`;
+    }
+  });
+}
+
+// Add item function
 
 function addItem(e) {
-  e.stopDefault();
-  console.log(1);
+  e.preventDefault();
+
+  // Get input value
+
+  let newItem =
+    document.querySelector(`.form-inline .form-control`).value + ` `;
+
+  // Create new li element
+
+  let li = document.createElement(`li`);
+
+  // Add class
+
+  li.className = `list-group-item`;
+
+  // Add text node with input value
+
+  li.appendChild(document.createTextNode(newItem));
+
+  itemList.appendChild(li);
+
+  // Add delete button
+
+  let deleteBtn = document.createElement(`button`);
+
+  // Add classes to delete button
+
+  deleteBtn.className = `btn btn-danger btn-sm float-right delete`;
+
+  // Append text node
+
+  deleteBtn.appendChild(document.createTextNode(`X`));
+
+  // Append deleteBtn
+
+  li.appendChild(deleteBtn);
+
+  // Append li to list
+
+  itemList.appendChild(li);
 }
