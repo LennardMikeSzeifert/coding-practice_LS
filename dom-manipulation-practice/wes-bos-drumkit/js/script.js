31084c8d187d24726e6a911a5b4bc1cdba9d03cd
codeandtranslate <123456789+translatetodeutsch@gmail.com>
2025-10-22 09:34:44 +0200

    feat(js): complete full functionality of Drum Kit
    Includes:
    - Connect div keys to corresponding audio files.
    - Rewind audio to the start after each key press.
    - Add key press animation for visual feedback.
    - Remove animation after transition ends.
    
    Refs: dom-manipulation-practice/wes-bos-drumkit/
    Authored-by: Lennard
    
    Note: This commit includes multiple related changes.
    In future commits, will aim to keep them more atomic
    to align with best practices.

--- dom-manipulation-practice/wes-bos-drumkit/js/script.js
+++ dom-manipulation-practice/wes-bos-drumkit/js/script.js
@@ -1,3 +1,47 @@
-window.addEventListener(`keydown`, function (e) {
-  console.log(e.code);
-});
+// window.addEventListener(`keydown`, function (e) {
+//   const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+//   const key = document.querySelector(`div[data-key="${e.code}"]`);
+//   if (!audio) {
+//     return;
+//   }
+//   audio.currentTime = `0`; // rewind audio to the start
+//   audio.play();
+//   key.classList.add(`playing`);
+
+//   function removeTransition(e) {
+//     if (e.propertyName !== `transform`) return; // skip it if it's not a transform
+
+//     this.classList.remove(`playing`);
+//   }
+
+//   const keys = document.querySelectorAll(`.key`);
+//   keys.forEach((key) =>
+//     key.addEventListener(`transitionend`, removeTransition)
+//   );
+// });
+
+// Cleaner version:
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  const key = document.querySelector(`div[data-key="${e.code}"]`);
+  if (!audio) {
+    return;
+  }
+  audio.currentTime = `0`; // rewind audio to the start
+  audio.play();
+  key.classList.add(`playing`);
+
+  function removeTransition(e) {
+    if (e.propertyName !== `transform`) return; // skip it if it's not a transform
+
+    this.classList.remove(`playing`);
+  }
+
+  const keys = document.querySelectorAll(`.key`);
+  keys.forEach((key) =>
+    key.addEventListener(`transitionend`, removeTransition)
+  );
+}
+
+window.addEventListener(`keydown`, playSound);
