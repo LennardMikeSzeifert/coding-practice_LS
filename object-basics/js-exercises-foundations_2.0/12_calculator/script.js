7d7585fe1d94357fd8133cb5e215efc784fadb51
codeandtranslate <123456789+translatetodeutsch@gmail.com>
2025-12-31 14:41:21 +0100

    feat: complete calculator exercises
    Fill out functions to:
    - Take two numbers and return their sum.
    - Take two numbers and return the result of subtracting the second from the first.
    - Take an array of numbers and return their sum.
    - Take an array of numbers and return their product.
    - Take a base and exponent and return the power result.
    - Given a single number, return its factorial.
    
    Refs: object-basics/js-exercises-foundations_2.0/12_calculator/
    Authored-by: Lennard

---
+++ object-basics/js-exercises-foundations_2.0/12_calculator/script.js
@@ -0,0 +1,37 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const sum = function (array) {
+  return array.reduce((total, current) => (total += current), 0);
+};
+
+const multiply = function (array) {
+  return array.reduce((total, current) => (total *= current), 1);
+};
+
+const power = function (base, exponent) {
+  return base ** exponent;
+};
+
+const factorial = function (num) {
+  const factoArr = [];
+  for (let i = num; i > 0; i--) {
+    factoArr.push(i);
+  }
+  return factoArr.reduce((product, current) => (product *= current), 1);
+};
+
+// Do not edit below this line
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
