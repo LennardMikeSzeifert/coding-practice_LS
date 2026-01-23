26972678559153d3f7d66ed2238dbc81ce4c6a58
codeandtranslate <123456789+translatetodeutsch@gmail.com>
2026-01-23 07:36:47 +0100

    feat(js): complete findTheOldest task
    - Implement `findTheOldest` function.
    - Use `.reduce()` to compare people by age.
    - Calculate ages for both the current person and the accumulator.
    - Return the person with the greatest age.
    
    Refs: object-basics/js-exercises-foundations_2.0/16_findTheOldest/
    Authored-by: Lennard

--- object-basics/js-exercises-foundations_2.0/16_findTheOldest/script.js
+++ object-basics/js-exercises-foundations_2.0/16_findTheOldest/script.js
@@ -1,4 +1,54 @@
-const findTheOldest = function () {};
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
 
-// Do not edit below this line
-module.exports = findTheOldest;
+const findTheOldest = function (peopleArr) {
+  const oldestPerson = peopleArr.reduce((acc, curr) => {
+    let currAge;
+    let accAge;
+    function calculateAge() {
+      if (!curr.yearOfDeath) {
+        currAge = new Date().getFullYear() - curr.yearOfBirth;
+      } else {
+        currAge = curr.yearOfDeath - curr.yearOfBirth;
+      }
+
+      if (!acc.yearOfDeath) {
+        accAge = new Date().getFullYear() - acc.yearOfBirth;
+      } else {
+        accAge = acc.yearOfDeath - acc.yearOfBirth;
+      }
+    }
+
+    function compareAges() {
+      if (currAge > accAge) {
+        acc = curr;
+      }
+    }
+    calculateAge();
+    compareAges();
+
+    return acc;
+  }, peopleArr[0]);
+  return oldestPerson;
+};
+
+console.log(findTheOldest(people));
+
+console.log(people);
+
+// // Do not edit below this line
+// module.exports = findTheOldest;
